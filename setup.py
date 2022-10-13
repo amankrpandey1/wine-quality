ca217eaafac0cd4c937fa8ee11380d44f008fb29
Aman Pandey <lazzzy@Amans-MacBook-Air.local>
2022-10-14 02:12:36 +0530

    setup done

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="lazzzy",
+    description="its a wine quality package",
+    packages = find_packages(),
+    license="MIT"   
+)
